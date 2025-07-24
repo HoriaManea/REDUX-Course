9648bd7e858e9eb55e90a4933abc9f1deb8c1fc1
Horia Manea <hmanea@c4sam.com>
2025-07-24 14:59:07 +0300

    feat(redux-toolkit): add redux toolkit and refactorize code using toolkit, add middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
-const { createStore, combineReducers } = require("redux");
-import customerReducer from "./features/customer/customerSlice";
-import accountReducer from "./features/account/accountSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer);
 
 export default store;
